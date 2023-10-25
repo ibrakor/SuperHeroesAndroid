72ec856502dd4e62c125e7169b9496fbc9834404
Ibra (Clase) <ibrahimkorsan@live.fr>
2023-10-25 20:40:06 +0200

    starting detail

--- app/src/main/java/com/ibrakor/superheroes/features/list/presentation/SuperHeroViewHolder.kt
+++ app/src/main/java/com/ibrakor/superheroes/features/list/presentation/SuperHeroViewHolder.kt
@@ -1,5 +1,7 @@
 package com.ibrakor.superheroes.features.list.presentation
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.faltenreich.skeletonlayout.Skeleton
@@ -7,20 +9,22 @@ import com.ibrakor.ejercicioformulario02.app.extensions.setUrl
 import com.ibrakor.superheroes.databinding.ViewSuperHeroItemBinding
 import com.ibrakor.superheroes.features.list.domain.SuperHeroOutput
 
-class SuperHeroViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+class SuperHeroViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
     private lateinit var binding: ViewSuperHeroItemBinding
-    private lateinit var skeleton: Skeleton
 
-    fun bind(model: SuperHeroOutput){
-        binding= ViewSuperHeroItemBinding.bind(view)
+
+    fun bind(model: SuperHeroOutput) {
+        binding = ViewSuperHeroItemBinding.bind(view)
 
         binding.apply {
             superheroImage.setUrl(model.superHero.imgUrl)
-            superheroFullname.text=model.biography.fullName
-            superheroName.text=model.superHero.name
-            superheroWork.text=model.work.occupation
-
+            superheroFullname.text = model.biography.fullName
+            superheroName.text = model.superHero.name
+            superheroWork.text = model.work.occupation
+            view.setOnClickListener {
+                (view.context as Activity).startActivity(Intent(Activity::class.java, De))
+            }
         }
     }
 
