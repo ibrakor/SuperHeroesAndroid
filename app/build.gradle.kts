6a4d84dca34d49a3705e775fcf588c6881f733b4
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2023-11-21 22:55:28 +0100

    Añadidas librerias Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -53,15 +55,6 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.10.1")
     //ViewModelScope
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    //Fragment
-    val fragment_version = "1.6.2"
-    implementation("androidx.fragment:fragment-ktx:$fragment_version")
-    //Navigation Component
-    val nav_version = "2.7.5"
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
